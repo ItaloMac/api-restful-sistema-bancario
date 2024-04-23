8ba9b8c611b2739028ed81c9dc194cebe7140d72
italo macedo <macedoitalo21@gmail.com>
2024-04-23 18:22:34 -0500

    Pré desenvolvimento das requisições relacionadas a contas

---
+++ src/controladores/contas.js
@@ -0,0 +1,30 @@
+const fs = require('fs/promises');
+const path = require('path');
+const { contas } = require('../bancodedados')
+
+function listarContasBancarias (req, res) {
+
+}
+
+function criarContaBancaria(req, res) {
+    
+}
+
+function atualizarUsuario (req, res) {
+   
+}
+
+function deletarConta (req, res) {
+   
+}
+
+
+
+
+module.exports = {
+    listarContasBancarias,
+    criarContaBancaria,
+    atualizarUsuario,
+    deletarConta
+    
+}
