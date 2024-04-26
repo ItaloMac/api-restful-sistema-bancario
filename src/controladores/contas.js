13cad9bec70098a179a10ebf88603ae3168aeb6d
italo macedo <macedoitalo21@gmail.com>
2024-04-26 13:26:08 -0500

    Requisição listarContasBancarias

--- src/controladores/contas.js
+++ src/controladores/contas.js
@@ -3,7 +3,14 @@ const path = require('path');
 const { contas } = require('../bancodedados')
 
 function listarContasBancarias (req, res) {
-
+    if (contas.length < 0) {
+        return res.status(200).json("Nenhuma conta encontrada")
+    } else {
+        return res.status(200).json({
+            message: contas.length + " contas encontradas",
+            contas: contas,
+        });
+    }
 }
 
 function criarContaBancaria(req, res) {
