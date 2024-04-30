579ec7082d89c2cc48bb77b3d9e0aa5052d5ae6c
italo macedo <macedoitalo21@gmail.com>
2024-04-30 19:36:03 -0300

    middleware - verificaSaldo

---
+++ src/middlewares/verificaSaldo.js
@@ -0,0 +1,28 @@
+const { contas } = require("../bancodedados");
+
+function verificaSaldo(req,res, next) {
+    const numero_conta = req.query.numero_conta
+    const senha = req.query.senha
+
+    if (!numero_conta ||  !senha ) {
+        return res.json({mensagem: "O número da conta, valor e senha são obrigatórios!"})
+    }
+
+    console.log(numero_conta, senha)
+
+    const conta = contas.find(conta => conta.numero === numero_conta)
+    if (conta == undefined) {
+        return res.json({mensagem: "Conta bancária não encontrada!"})
+    }
+
+
+    const senhaConta = conta.usuario.senha
+
+    console.log(senhaConta)
+
+    if (senha != senhaConta) {
+        return res.json({mensagem: "A senha informada não corresponde a senha cadastrada na conta!"})
+    }
+
+    next()
+}
